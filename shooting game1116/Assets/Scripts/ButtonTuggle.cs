dcc80b9f1f858aa02991adcb553d47493ffa1cf1
Ryu_tatsu <Ryu_tatsu55@yahoo.co.jp>
2017-11-16 20:43:44 +0900

    追加方法分からないので新規で追加 11/16時点での進捗

---
+++ shooting game1116/Assets/Scripts/ButtonTuggle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class ButtonTuggle : MonoBehaviour {
+//	PlayerLife HP;
+	//GameObject player;
+
+
+
+	// Use this for initialization
+	//void Start () {
+		//player = GameObject.Find ("Player");
+		//HP = player.GetComponent<PlayerLife> ();
+		
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+	//	int buttonHP = HP.Life;
+		//if(buttonHP <= 0){
+			//GameObject.Find ("GameOver").GetComponent<GameOverButton> ().SetActive = true;
+
+		
+	//}
+//}
---
+++ shooting game1116/Assets/Scripts/GameOverButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameOverButton : MonoBehaviour {
+	GameObject GameOver;
+
+	// Use this for initialization
+	void  Start () {
+		//transform.GetComponent<Image>.enabled = false;
+		gameObject.SetActive (false);
+
+
+	}
+	
+	// Update is called once per frame
+	public void Lose () {
+		//gameObject.GetComponent<Text>().enabled = true;
+
+
+		}
+	public void PushGameoverButton(){
+		SceneManager.LoadScene ("Titlescene");
+	}
+
+}
+
+//非表示
+//体力が0になる
+//表示
+//UIボタンでタイトルへ戻るがでる
+//押すとタイトルシーンへ移動
+//非表示
+
+
+//わからないこと
+//ゲーム開始時点で非表示のオブジェクト(ボタン)を任意タイミングで表示する方法
+
+
+//GameoverスクリプトでLose メソッドを書いてそこでオブジェクトを有効化する?
+//→非表示オブジェクトについているスクリプトは無効化されている。どうやって有効化する？
+//SetActiveでfalseにしたオブジェクトを外部からtrueにする　オブジェクトを変数として保持しておく
