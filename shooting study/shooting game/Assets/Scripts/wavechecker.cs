fbd76e1c89c042728788c9c0f1b5da060b1c069f
Ryu_tatsu <Ryu_tatsu55@yahoo.co.jp>
2017-12-01 13:48:34 +0900

    wavecheckerが追加されていない修正

---
+++ shooting study/shooting game/Assets/Scripts/wavechecker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wavechecker : MonoBehaviour {
+
+	public GameObject Enemy;
+
+	public void Invokeset(){
+			Invoke ("wavecherck", 5);
+		}
+		
+
+	void wavecherck(){
+		Debug.Log ("ok");
+		Instantiate (Enemy, new Vector2 (0, 3.44f),Quaternion.identity);
+	}
+
+}
+//ステージクリア条件
+//敵を一定数倒した時
+//一定秒数間生き残る
+
