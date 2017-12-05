fe12ec578c54e710416b603f6bb80fee982c2b19
Ryu_tatsu <Ryu_tatsu55@yahoo.co.jp>
2017-12-05 16:05:59 +0900

    #4.5 追加されていないスクリプトを追加　score,GameManager

---
+++ shooting study/shooting game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	//追々難易度を自動判定して、以下を設定するようにする
+	public int EnemySpawnCount;
+	public int DestoroyScore;
+	public int WaveCount;
+	Score d;
+	public Text score;
+		
+	void Start(){
+		//敵の湧き上限
+		EnemySpawnCount = 18;
+		//表示するスコア
+		DestoroyScore = 0;
+		WaveCount = 0;
+	}
+	void Update(){
+		//現在のスコアを表示→バグ発生中　テキストにうまく表示されない　次回修正
+		score = GameObject.Find("NowScore").GetComponent<Text> ();
+		d = GameObject.Find ("NowScore").GetComponent<Score> ();
+		d.NowScoreSet ();
+	}
+
+	public void CountCheck(){
+		//湧き数を減算
+		EnemySpawnCount -= 1;
+		DestoroyScore += 100;
+		WaveCount += 1;
+	}
+}
+
+
+//敵の湧き数上限設定、湧き数上限から減算処理(Invokeしたら減算→wavecheckerを参照)
+//湧き数が0になるとgameclearスクリプトを呼び出し
+
+
+//プレイヤーの残機の設定
+//残機が0になるとgameoverスクリプトを呼び出し
+
---
+++ shooting study/shooting game/Assets/Scripts/Score.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+	public Text score;
+	public Text Nscore;
+	public Text damage;
+	public int lastscore;
+	public int nowscore;
+	public int lastTakeDamage;
+
+	// Use this for initialization
+	void Start () {
+		lastscore = 0;
+		nowscore = 0;
+		score = GameObject.Find("Score").GetComponent<Text> ();
+		Nscore = GameObject.Find ("NowScore").GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void ScoreSet(){
+		//ここを変えるぞ//////////////////////////////////////
+		lastscore = GameObject.Find ("GameManager").GetComponent<GameManager> ().DestoroyScore;
+
+		score.text = "Score:" + lastscore.ToString ();
+	}
+	public void TakenDamageSet(){
+		damage = GameObject.Find("TakenDamage").GetComponent<Text> ();
+		lastTakeDamage = GameObject.Find ("Player").GetComponent<PlayerController> ().TakenDamage;
+
+		damage.text = "受けたダメージ:" + lastTakeDamage.ToString ();
+	}
+	public void NowScoreSet(){
+		nowscore = GameObject.Find ("GameManager").GetComponent<GameManager> ().DestoroyScore;
+		Nscore.text = "Score:" + nowscore.ToString ();
+	}
+}
